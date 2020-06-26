559097ab2152e9f3e5b6cd2b873727b95bc56f3d
Reza Rafsanjani <rr8eezz4aa@gmail.com>
2020-06-26 22:59:45 +0430

    fix urls

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createUrl() {
     var search = document.getElementById("search");
     var searchTxt = search.value;
-    var searchUrl = "file:///e:/tmp/gifm/go.html?q=" + searchTxt;
+    var searchUrl = "https://rr8eezz4aa.github.io/GIFM/go.html?q=" + searchTxt;
     document.getElementById("result").style.display = "flex";
     document.getElementById("resultUrl").value = searchUrl
 }
