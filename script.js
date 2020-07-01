03c57bcded00c3d402ac349b7e099043c7081213
Reza Rafsanjani <rr8eezz4aa@gmail.com>
2020-07-01 15:21:36 +0430

    encode created url

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function createUrl() {
     var search = document.getElementById("search");
     var searchTxt = search.value;
-    var searchUrl = "https://rr8eezz4aa.github.io/GIFM/go.html?q=" + searchTxt;
-    document.getElementById("result").style.display = "flex";
-    document.getElementById("resultUrl").value = searchUrl
+    if (searchTxt) {
+        var searchUrl = "https://rr8eezz4aa.github.io/GIFM/go.html?q=" + searchTxt;
+        var searchUrl = encodeURI(searchUrl);
+        document.getElementById("result").style.display = "flex";
+        document.getElementById("resultUrl").value = searchUrl
+    }
 }
 
 function goToUrl() {
